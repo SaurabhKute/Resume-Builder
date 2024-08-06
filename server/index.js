638c8656ac2b66c2b894acc006fa34e144390ccd
SaurabhKute <saurabhkute321@gmail.com>
2024-08-06 15:57:18 +0530

    backend: auth and error handling changes

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import cors from "cors";
 import connectDatabase from "./src/config/db.js";
 import bodyParser from "body-parser";
-
 import dotenv from "dotenv";
 import chalk from "chalk";
 import routes from './src/routes/index.js';
+import errorConverter from "./src/middlewares/errorConverter.js";
+import errorHandler from "./src/middlewares/errorHandler.js";
 
 dotenv.config();
 
@@ -16,9 +17,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.json());
 
-
+// Define routes
 app.use('/v1', routes);
 
+// Error handling middleware
+app.use(errorConverter);
+app.use(errorHandler);
 
 // Connect to the database and seed data
 (async () => {
